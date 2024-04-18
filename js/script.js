3cd86ffd521b9749e6dab19b6125f72a27f2c088
doko972 david <doko972@gmail.com>
2024-04-18 09:56:48 +0200

    modify menu with scrolling menu

--- js/script.js
+++ js/script.js
@@ -1,28 +1,49 @@
 //Galerie//
+// const gallery = document.querySelector('.gallery__img');
+// let isDragging = false;
+// let lastX;
+
+// gallery.addEventListener('mousedown', (event) => {
+//   isDragging = true;
+//   lastX = event.clientX;
+// });
+
+// gallery.addEventListener('mouseup', () => {
+//   isDragging = false;
+// });
+
+// gallery.addEventListener('mouseleave', () => {
+//   isDragging = false;
+// });
+
+// gallery.addEventListener('mousemove', (event) => {
+//   if (isDragging) {
+//     const deltaX = event.clientX - lastX;
+//     lastX = event.clientX;
+
+//     gallery.style.transform = `rotateY(${deltaX}deg)`;
+//   }
+// });
+
 const gallery = document.querySelector('.gallery__img');
-let isDragging = false;
-let lastX;
+let isMouseDown = false;
+let lastMouseX = 0;
 
 gallery.addEventListener('mousedown', (event) => {
-  isDragging = true;
-  lastX = event.clientX;
+  isMouseDown = true;
+  lastMouseX = event.clientX;
 });
 
-gallery.addEventListener('mouseup', () => {
-  isDragging = false;
+document.addEventListener('mouseup', () => {
+  isMouseDown = false;
 });
 
-gallery.addEventListener('mouseleave', () => {
-  isDragging = false;
-});
-
-gallery.addEventListener('mousemove', (event) => {
-  if (isDragging) {
-    const deltaX = event.clientX - lastX;
-    lastX = event.clientX;
+document.addEventListener('mousemove', (event) => {
+  if (isMouseDown) {
+    const mouseX = event.clientX;
+    const deltaX = mouseX - lastMouseX;
+    lastMouseX = mouseX;
 
     gallery.style.transform = `rotateY(${deltaX}deg)`;
   }
 });
-
-
